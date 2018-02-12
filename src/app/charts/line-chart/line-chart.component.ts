641f6693db71f24f9b7c8171aadd01dff32167e3
shaneswezey <shanedswezey@gmail.com>
2018-02-11 23:57:39 -0800

    Created sample data of line chart

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../shared/chart.colors';
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  { data:[32, 13, 46, 23, 38, 56], label: 'Sentiment Analysis' },
+  { data:[12, 18, 26, 13, 28, 26], label: 'Image Recognition' },
+  { data:[52, 34, 49, 53, 68, 66], label: 'Forecasting' }
+];
+
+const LINE_CHART_LABELS: string[] = ['JAN', 'FEB', 'MAR', 'APR', 'JUN', 'JUL'];
+
 
 @Component({
   selector: 'app-line-chart',
@@ -6,6 +16,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./line-chart.component.css']
 })
 export class LineChartComponent implements OnInit {
+
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true
+  };
+  lineChartLegend: true;
+  lineChartType =  'line';
+  lineChartColors = LINE_CHART_COLORS;
+
 
   constructor() { }
 
